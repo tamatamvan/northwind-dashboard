03d34ee5c670516a89b5344af676ed216790dd4a
Septian A Tama <sfirst.tama@gmail.com>
2025-05-27 17:41:19 +0700

    fetch dto and configure project

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
+import path from 'path';
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    react(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+      react(),
+    ],
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      proxy: {
+        '/api':
+          env.VITE_REST_API_URL ||
+          'http://ec2-34-201-46-215.compute-1.amazonaws.com',
+      },
+    },
+  };
 });
