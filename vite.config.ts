d0bc2d61126b4286da760b2fba7bc0aed987555d
Septian A Tama <sfirst.tama@gmail.com>
2025-05-27 17:47:32 +0700

    setup tailwindcss and shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
@@ -12,6 +13,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
       react(),
+      tailwindcss(),
     ],
     resolve: {
       alias: {
